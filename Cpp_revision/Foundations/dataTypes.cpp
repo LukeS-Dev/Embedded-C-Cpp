b3fc7f81f86faeeb95c0c3611ad7bb1ca19c7557
Luke Su <luke.su02@gmail.com>
2022-09-28 08:25:45 +1000

    Standard data type information added

---
+++ Cpp_revision/Foundations/dataTypes.cpp
@@ -0,0 +1,39 @@
+#include <iostream> 
+#include <string> 
+
+bool trueExpression = true;
+bool falseExpression = false;
+
+//Standard types such as Bool,Int,Float, double... ETC are supported
+
+//Strings are not supported in c native
+//However C++ stdlib supports this
+
+//Pointers Special data type for memory addresses.
+//Pointers are refs to variables
+//Same pointer may be used to access many different variables sequentially
+
+//C++ Reference for types -> Basic concepts -> Fundamental types.
+
+int dec = 100;
+int hex = 0x3A;
+int bin = 0b1100;
+int units = 23U;
+
+float decimalFloat = 23.0f;
+
+double doubleDefault = 1.5;
+
+char nullChar = '\0';
+char newLine = '\n';
+
+int main()
+{
+    //Anything here is a local variable, the data outside are
+    //Global variables.
+    int test = 0;
+
+    /* This is a multi-line commment
+    decloration.    
+    */
+}
